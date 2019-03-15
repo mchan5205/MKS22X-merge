e9b576c5bb226c551a7fc2d9540c6884c8052304
Michael Chan <mchan10@stuy.edu>
2019-03-15 10:59:17 -0400

    created the splitting into 2 arys

--- Merge.java
+++ Merge.java
@@ -1,5 +1,18 @@
 public class Merge{
   public static void mergesort(int[] data){
-    
+    mergesorth(data, 0, data.length - 1);
+  }
+  private static void mergesorth(int[] data, int low, int high){
+    if (low >= high){
+      return;
+    }
+    int[] lary = new int[data.length / 2];
+    System.out.println(lary.length);
+    int[] hary = new int[data.length - (data.length / 2)];
+    System.out.println(hary.length);
+  }
+  public static void main(String[] args){
+    int[] data = {80, 60, 90, 50, 70};
+    mergesort(data);
   }
 }
