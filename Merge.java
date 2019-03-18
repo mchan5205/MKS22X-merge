9dc8dd7b9e1b0758ddb2ac2da1565e98a8fc3e06
Michael Chan <mchan10@stuy.edu>
2019-03-18 11:02:04 -0400

    merge mostly working

--- Merge.java
+++ Merge.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Merge{
   public static void mergesort(int[] data){
     mergesorth(data, 0, data.length - 1);
@@ -7,22 +8,37 @@ public class Merge{
       return;
     }
     int[] lary = new int[data.length / 2];
-    System.out.println(lary.length);
     int[] hary = new int[data.length - (data.length / 2)];
-    System.out.println(hary.length);
   }
   public static void main(String[] args){
     int[] data = {80, 60, 90, 50, 70};
+    int[] data1 = {80};
+    int[] data2 = {60};
+    merge(data1, data2, 0, data);
+    System.out.println(Arrays.toString(data));
     mergesort(data);
   }
-  public static void merge(int[] data1, int[] data2){
+  public static void merge(int[] data1, int[] data2, int low, int[] data){
     int l = 0;
     int h = 0;
     for (int i = 0; i < data1.length + data2.length; i++){
       if (l < data1.length && h < data2.length){
         if (data1[l] > data2[h]){
-          
+          data[low + i] = data2[h];
+          h += 1;
         }
+        else{
+          data[low + i] = data1[l];
+          l += 1;
+        }
+      }
+      else if (l < data1.length){
+        data[low + i] = data1[l];
+        l += 1;
+      }
+      else{
+        data[low + i] = data2[h];
+        h += 1;
       }
     }
   }
