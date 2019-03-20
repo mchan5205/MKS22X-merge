2516c7cbb9cccfbac90b6a88a7f39262ad55dea4
Michael Chan <mchan10@stuy.edu>
2019-03-20 00:12:53 -0400

    added insertionsort optimization

--- Merge.java
+++ Merge.java
@@ -19,7 +19,7 @@ public class Merge{
       lary[i] = data[low + i];
       i++;
     }
-    System.out.println(Arrays.toString(lary) + " lary");
+    //System.out.println(Arrays.toString(lary) + " lary");
     int[] hary = new int[(high - low) / 2];
     int h = 0;
     while (h < hary.length){
@@ -27,7 +27,13 @@ public class Merge{
       h++;
       i++;
     }
-    System.out.println(Arrays.toString(hary) + " hary");
+    if (high - low < 10){
+      insertionSort(lary);
+      insertionSort(hary);
+      merge(lary,hary,low,data);
+      return;
+    }
+    //System.out.println(Arrays.toString(hary) + " hary");
     if (lary.length != 1){
       mergesorth(data, low, low + lary.length);
     }
@@ -47,14 +53,42 @@ public class Merge{
       o++;
     }
     merge(lary,hary,low,data);
-    System.out.println(Arrays.toString(data));
+    //System.out.println(Arrays.toString(data));
   }
-  public static void main(String[] args){
-    int[] data = {38, 27, 43, 3, 9, 82, 10};
-    System.out.println(Arrays.toString(data));
-    mergesort(data);
-    System.out.println(Arrays.toString(data));
+  public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Merge.mergesort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
   }
+}
   public static void merge(int[] data1, int[] data2, int low, int[] data){
     int l = 0;
     int h = 0;
@@ -77,6 +111,23 @@ public class Merge{
         data[low + i] = data2[h];
         h += 1;
       }
+    }
+  }
+  public static void insertionSort(int[] ary){
+    for (int i = 0; i < ary.length; i++){
+      int temp = ary[i];
+      int x = i - 1;
+      boolean done = false;
+      while (x >= 0 && ! done){
+        if (ary[x] > temp){
+          ary[x + 1] = ary[x];
+          x = x - 1;
+        }
+        else{
+          done = true;
+        }
+      }
+      ary[x + 1] = temp;
     }
   }
 }
